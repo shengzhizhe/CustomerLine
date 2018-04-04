70bddf37e16c2140af7853e19b823422fc9c63b2
xuesemofa@126.com <xuesemofa@126.com>
2018-04-04 15:20:08 +0800

    增加filter过滤器，用来应对自定义头部信息后的options请求

--- src/main/java/org/client/com/CorsFilter.java
+++ src/main/java/org/client/com/CorsFilter.java
@@ -25,7 +25,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods",
-                "POST, GET, OPTIONS, DELETE");
+                "POST, GET, PUT,OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Content-Type, x-requested-with, X-Custom-Header, Authorization");
